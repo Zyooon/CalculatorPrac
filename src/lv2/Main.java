417558c0204cbea1416bde4f019d16a0a6d8dc9a
Zyooon <wcw7373@gmail.com>
2025-04-16 14:01:49 +0900

    계산기 lv1

---
+++ src/lv2/Main.java
@@ -0,0 +1,101 @@
+package lv2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static int firstNum;
+    static int secondNum;
+    static char operator;
+    static boolean isOnOff = true;
+    static Set<Character> operatorSet = new HashSet<>(Arrays.asList('*', '+', '-', '/'));
+    static int resultNum;
+    static String exit;
+
+    public static void main(String[] args) {
+
+        //메인 클래스
+        //생성자 클래스
+        //숫자 두개, 연산자, 결과 값, 결과 기록 stack
+        //연산하는 클래스
+
+        System.out.println("지금부터 계산을 시자악 하겠습니다~!");
+
+        while(isOnOff){
+
+            //첫번째 숫자 입력
+            while(true){
+                System.out.println("첫번째 숫자를 입력하세요");
+                System.out.print("숫자 입력 : ");
+                try{
+                    firstNum = sc.nextInt();
+                    break;
+                } catch (Exception e) {
+                    System.out.println("제대로 입력해 주세요");
+                    sc.nextLine();
+                }
+            }
+
+            //연산자 입력
+            while(true){
+                System.out.println("연산자를 입력하세요");
+                System.out.print("연산자 입력 : ");
+                try{
+                    operator = sc.next().charAt(0); //nextLine > next 로 변경
+                    if(operatorSet.contains(operator)) break;
+
+                } catch (Exception e) {
+                    System.out.println("제대로 입력해 주세요1");
+                    sc.nextLine();
+                }
+                System.out.println("제대로 입력해 주세요2");
+            }
+
+            //두번째 숫자 입력
+            while(true){
+                System.out.println("두번째 숫자를 입력하세요");
+                System.out.print("숫자 입력 : ");
+                try{
+                    secondNum = sc.nextInt();
+                    if(operator == '/' && secondNum == 0) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        continue;
+                    }
+                    break;
+                } catch (Exception e) {
+                    System.out.println("제대로 입력해 주세요");
+                    sc.nextLine();
+                    continue;
+                }
+            }
+
+            //계산하기
+            switch (operator){
+                case '+' : resultNum = firstNum + secondNum; break;
+                case '-' : resultNum = firstNum - secondNum; break;
+                case '*' : resultNum = firstNum * secondNum; break;
+                case '/' : resultNum = firstNum / secondNum; break;
+            }
+
+            System.out.println("연산 결과 : " + resultNum);
+            System.out.println("\r더 하시려면 아무키나 눌러주세요. (exit 입력 시 종료)");
+
+            try{
+                exit = sc.next();
+                if(exit.equals("exit")){
+                    break;
+                }
+            } catch (Exception e) {
+                sc.nextLine();
+            }
+            //계산 끝
+        }
+        System.out.println("종료되었습니다.");
+        sc.close();
+
+    }
+}
---
+++ src/lv3/Main.java
@@ -0,0 +1,36 @@
+package lv3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+
+    public Main() {
+        setTitle("500x300 프레임과 컨텐트팬 만들기"); // 프레임 제목 설정
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 300); // 프레임 크기 설정
+
+        Container contentPane = getContentPane(); //프레임에서 컨텐트팬 받아오기
+        contentPane.setLayout(null);
+
+        JLabel lblNewLabel = new JLabel("레이블 테스트 입니다");
+        lblNewLabel.setBounds(182, 133, 124, 15); // 레이블 위치 설정
+        contentPane.add(lblNewLabel); // 콘텐트팬에 레이블 붙이기
+
+        JButton btnNewButton = new JButton("버튼 테스트");
+        btnNewButton.setBounds(182, 59, 110, 23);
+        contentPane.add(btnNewButton);
+
+        JTextField textField = new JTextField();
+        textField.setBounds(190, 203, 96, 21);
+        contentPane.add(textField);
+        textField.setColumns(10); // 텍스트 필드 기본 입력문자 갯수
+
+        setVisible(true); //화면에 프레임 출력
+    }
+
+    public static void main(String[] args) {
+        Main mf = new Main();
+
+    }
+}
